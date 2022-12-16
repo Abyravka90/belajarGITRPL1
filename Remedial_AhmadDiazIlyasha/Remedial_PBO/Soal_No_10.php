4d4d7082b2129061d7617304ceea7a92e63ce05e
ahmaddiaz <AhmadDiaz089@gmail.com>
2022-12-16 11:20:21 +0700

    remedial

---
+++ Remedial_AhmadDiazIlyasha/Remedial_PBO/Soal_No_10.php
@@ -0,0 +1,13 @@
+<?php
+function persegipanjang{
+        $panjang    =
+        $lebar      =
+    
+        $luas_persegi_panjang = $panjang*$lebar;
+
+        $keliling_persegi_panjang = 2*($panjang+$lebar);
+        
+        echo "Maka luas persegi panjang sama dengan [ $panjang x $lebar ] = $luas_persegi_panjang<br />";
+        echo "Maka keliling persegi panjang sama dengan [ 2 x ($panjang + $lebar) ] = $keliling_persegi_panjang";
+    }
+?>
