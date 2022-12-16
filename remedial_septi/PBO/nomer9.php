7d968e611d39c26c7801d012cbaa7c18ef324944
najlaalifah1165 <najlaalifah1165@gmail.com>
2022-12-16 19:15:46 +0700

    remedial

---
+++ remedial_septi/PBO/nomer9.php
@@ -0,0 +1,12 @@
+<?php
+
+$people = array('nama' => 'Septi','nis' => '262723','nilai' => '80');
+
+echo $people['nama'];
+echo "\n";
+echo $people['nis'];
+echo "\n";
+echo $people['nilai'];
+echo "\n";
+
+?>
